d01623ebe60fdc657383fae473e5d71449a96e6a
jacobsidhu <jacobsidhu100@gmail.com>
2026-08-20 13:56:22 +0100

    Configure S3 Terraform backend

--- infra/terraform/terraform.tf
+++ infra/terraform/terraform.tf
@@ -1,6 +1,12 @@
 terraform {
   required_version = ">= 1.6.0"
 
+  backend "s3" {
+    bucket = "jacobsidhu"
+    key    = "portfolio/terraform.tfstate"
+    region = "eu-west-2"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
