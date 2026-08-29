55935b158a14be5a34d8985051896df5388b4e58
jacobsidhu <jacobsidhu100@gmail.com>
2026-08-29 11:37:43 +0100

    Chore: Resolved refer

--- infra/terraform/lambda.tf
+++ infra/terraform/lambda.tf
@@ -6,7 +6,7 @@ data "archive_file" "lambda_zip" {
 
 resource "aws_lambda_function" "portfolio_lambda" {
   function_name = "portfolio_lambda"
-  role          = aws_iam_role.portfolio_role_lambda_exec.arn
+  role          = aws_iam_role.lambda_exec.arn
 
   filename         = data.archive_file.lambda_zip.output_path
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
