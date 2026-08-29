975eb645dca4bd6d2686bbf098ee97634f3b5876
jacobsidhu <jacobsidhu100@gmail.com>
2026-08-29 13:24:58 +0100

    FEAT: add integration and route for api gateway

--- infra/terraform/api-gateway.tf
+++ infra/terraform/api-gateway.tf
@@ -4,7 +4,13 @@ resource "aws_apigatewayv2_api" "api" {
 }
 
 resource "aws_apigatewayv2_integration" "lambda_integration" {
-    api_id = aws_apigatewayv2_api.api.id
-    integration_type = "AWS_PROXY"
-    integration_uri = aws_lambda_function.portfolio_lambda.invoke_arn
+  api_id           = aws_apigatewayv2_api.api.id
+  integration_type = "AWS_PROXY"
+  integration_uri  = aws_lambda_function.portfolio_lambda.invoke_arn
+}
+
+resource "aws_apigatewayv2_route" "lambda_route" {
+  api_id    = aws_apigatewayv2_api.api.id
+  route_key = "POST/contact"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
 }
