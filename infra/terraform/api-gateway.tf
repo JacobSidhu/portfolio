60d39952f0eacb8ca7d183b094f4810418e1434c
jacobsidhu <jacobsidhu100@gmail.com>
2026-08-29 13:12:22 +0100

    FEAT: add integration lambda invoke uri

--- infra/terraform/api-gateway.tf
+++ infra/terraform/api-gateway.tf
@@ -2,3 +2,9 @@ resource "aws_apigatewayv2_api" "api" {
   name          = "portfolio-http-api-gtw"
   protocol_type = "HTTP"
 }
+
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+    api_id = aws_apigatewayv2_api.api.id
+    integration_type = "AWS_PROXY"
+    integration_uri = aws_lambda_function.portfolio_lambda.invoke_arn
+}
