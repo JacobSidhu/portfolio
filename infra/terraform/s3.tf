740674fd3bb188f9c5b18b8de942c2d5098f841f
jacobsidhu <jacobsidhu100@gmail.com>
2026-08-29 09:43:33 +0100

    ADD: enabled bucket web hosting and disabling public block access to the bucket

--- infra/terraform/s3.tf
+++ infra/terraform/s3.tf
@@ -1,3 +1,44 @@
 resource "aws_s3_bucket" "portfolio_bucket" {
   bucket = var.portfolio_bucket_name
 }
+resource "aws_s3_bucket_website_configuration" "portfolio_website" {
+  bucket = aws_s3_bucket.portfolio_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "portfolio_public_access" {
+  bucket = aws_s3_bucket.portfolio_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "portfolio_public_read" {
+  bucket = aws_s3_bucket.portfolio_bucket.id
+
+  depends_on = [
+    aws_s3_bucket_public_access_block.portfolio_public_access
+  ]
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.portfolio_bucket.arn}/*"
+      }
+    ]
+  })
+}
