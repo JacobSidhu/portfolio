7fbf49c77b0b3cfad22de4a5c3cfbfd164560f38
jacobsidhu <jacobsidhu100@gmail.com>
2026-08-29 11:21:09 +0100

    Init: Provisioning helloworld lambda function

--- infra/terraform/lambda.tf
+++ infra/terraform/lambda.tf
@@ -1,6 +1,13 @@
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/../../backend/lambda-functions/contact"
+  output_path = "${path.module}/lambda.zip"
+}
+
 resource "aws_lambda_function" "portfolio_lambda" {
     function_name = "portfolio_lambda"
     role          = aws_iam_role.portfolio_role_lambda_exec.arn
     handler       = "contact/index.handler"
     runtime       = "nodejs18.x"
+    filename      = data.archive_file.lambda_zip.output_path
 }
