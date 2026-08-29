b4cbffb8e74718e4dcec5557677c05fd17a94735
jacobsidhu <jacobsidhu100@gmail.com>
2026-08-29 11:28:37 +0100

    Chore: Corrected formatting issue

--- infra/terraform/lambda.tf
+++ infra/terraform/lambda.tf
@@ -7,7 +7,7 @@ data "archive_file" "lambda_zip" {
 resource "aws_lambda_function" "portfolio_lambda" {
     function_name = "portfolio_lambda"
     role          = aws_iam_role.portfolio_role_lambda_exec.arn
-    handler       = "contact/index.handler"
+    handler       = "index.handler"
     runtime       = "nodejs18.x"
     filename      = data.archive_file.lambda_zip.output_path
 }
