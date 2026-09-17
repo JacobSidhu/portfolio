749f69337a5b30b4df6b7299c31d1f4ae1bc70ca
jacobsidhu <jacobsidhu100@gmail.com>
2026-09-17 12:13:55 +0100

    ADD: Integration Method

--- infra/terraform/api-gateway.tf
+++ infra/terraform/api-gateway.tf
@@ -19,6 +19,7 @@ resource "aws_apigatewayv2_integration" "lambda_integration" {
 resource "aws_apigatewayv2_integration" "s3_index_integration" {
   api_id                 = aws_apigatewayv2_api.api.id
   integration_type       = "HTTP_PROXY"
+  integration_method     = "GET"
   integration_uri        = "http://${aws_s3_bucket_website_configuration.portfolio_website.website_endpoint}"
   payload_format_version = "1.0"
 }
